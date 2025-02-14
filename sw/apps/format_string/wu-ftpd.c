12782a48184e9d4c9f857b26fe33db81b937e517
Jonathan Dumaz <jonath.dumaz73@gmail.com>
2025-02-14 15:34:04 +0100

    Ajout du fichier pour tester l'attaque par format string

---
+++ sw/apps/format_string/wu-ftpd.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DIFT
+
+void secretFunction()
+{
+  printf("Congratulations!\n");
+  printf("You have entered in the secret function!\n");
+
+  exit(0);
+}
+
+void echo()
+{
+  int a;
+  register int i asm("x8");
+ 
+  a = i;
+
+  #ifdef DIFT
+  /*TAG INITIALIZATION*/
+  asm volatile ("p.spsw x0, 0(%[a]);"                
+               :
+               :[a] "r" (&a));
+  #endif
+
+  printf("%224u%n%35u%n%253u%n%n", 1, (int*) (a-4), 1, (int*) (a-3), 1, (int*) (a-2), (int*) (a-1));
+
+  return;
+}
+
+int main(int argc, char* argv[])
+{ 
+  volatile int a = 1;
+
+  if(a)
+    echo();
+  else
+    secretFunction();
+
+  return 0;
+}
